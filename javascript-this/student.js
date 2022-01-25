0368cb76decd0088dfd521a95c1b1c20a9d07bb9
Jeff Kwon <jykwon007@gmail.com>
2022-01-24 20:07:42 -0800

    Completed javascript-this exercise.

---
+++ javascript-this/student.js
@@ -0,0 +1,12 @@
+/* exported student */
+var student = {
+  firstName: 'Tina',
+  lastName: 'Kwon',
+  subject: 'JavaScript',
+  getFullName: function () {
+    return this.firstName + ' ' + this.lastName;
+  },
+  getIntroduction: function () {
+    return 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.';
+  }
+};
