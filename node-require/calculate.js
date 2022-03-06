447b63852eedf2709e448d0e9683fd863fd75819
Jeff Kwon <jykwon007@gmail.com>
2022-03-06 06:50:50 +0000

    turned string from integer to numbers.

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -8,11 +8,11 @@ const equation = process.argv[3];
 const y = process.argv[4];
 
 if (equation === 'plus') {
-  console.log('result:', add.sum(parseInt(x), parseInt(y)));
+  console.log('result:', add.sum(Number(x), Number(y)));
 } else if (equation === 'minus') {
-  console.log('result:', subtract.difference(parseInt(x), parseInt(y)));
+  console.log('result:', subtract.difference(Number(x), Number(y)));
 } else if (equation === 'times') {
-  console.log('result:', multiply.product(parseInt(x), parseInt(y)));
+  console.log('result:', multiply.product(Number(x), Number(y)));
 } else if (equation === 'over') {
-  console.log('result:', divide.quotient(parseInt(x), parseInt(y)));
+  console.log('result:', divide.quotient(Number(x), Number(y)));
 }
