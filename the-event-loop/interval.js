32a91b4718c56f65527cd7888cbfd85d91f1a2f0
Jeff Kwon <jykwon007@gmail.com>
2022-03-08 04:06:39 +0000

    fixed interval function to not fire off at same time.

--- the-event-loop/interval.js
+++ the-event-loop/interval.js
@@ -1,9 +1,10 @@
 let currentCount = 3;
 
 const intervalID = setInterval(function () {
-  console.log(currentCount--);
   if (currentCount === 0) {
     console.log('Blast off!');
     clearInterval(intervalID);
+  } else {
+    console.log(currentCount--);
   }
 }, 1000);
