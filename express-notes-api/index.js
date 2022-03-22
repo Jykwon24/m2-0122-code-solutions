cad60491f7178ceacd3d7854558b38d498d82dc8
Jeff Kwon <jykwon007@gmail.com>
2022-03-22 05:02:50 +0000

    Client GET notes by iD.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -5,9 +5,8 @@ const jsonMethod = express.json();
 app.use(jsonMethod);
 
 const data = require('./data.json');
-
+const dataArray = [];
 app.get('/api/notes', (req, res) => {
-  const dataArray = [];
   for (const key in data.notes) {
     if (!data.notes[key]) {
       res.status(200).json(dataArray);
@@ -18,6 +17,20 @@ app.get('/api/notes', (req, res) => {
   res.status(200).json(dataArray);
 });
 
-app.listen('3000', () => {
+app.get('/api/notes/:id', (req, res) => {
+  const iD = req.params.id;
+  if (isNaN(parseInt(iD))) {
+    res.status(400).send({ error: 'id must be a positive integer' });
+  } else if (data.notes[iD]) {
+    res.status(200).send(data.notes[iD]);
+  } else if (!data.notes[iD]) {
+    res.status(404).send({ error: `cannot find note with id ${iD}` });
+  }
+}
 
+);
+
+app.listen('3000', () => {
+  // eslint-disable-next-line no-console
+  console.log('listening on port 3000');
 });
